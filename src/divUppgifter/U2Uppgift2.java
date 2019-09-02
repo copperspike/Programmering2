e2c119e6569062b6f6f077a419d7e38c2f1eb816
SpikeYr <emil.f.anderson@gmail.com>
2019-09-02 10:53:28 +0200

    09-02 Uppgift 2

---
+++ src/divUppgifter/U2Uppgift2.java
@@ -0,0 +1,38 @@
+package divUppgifter;
+
+import java.util.Scanner;
+
+public class U2Uppgift2 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println(reverse(sc.nextLine()));
+
+		sc.close();
+
+	}
+
+	public static String reverse(String s) {
+		return reverse(s, 0);
+	}
+
+	public static String reverse(String s, int i) {
+		if (i >= s.length()) {
+			return s;
+		}
+
+		String temp = "";
+		if (i != 0) {
+			temp += s.substring(0, i);
+		}
+		temp += s.charAt(s.length() - 1);
+		
+		temp += s.substring(i, s.length() - 1);
+
+		return reverse(temp, i + 1);
+
+	}
+
+}
