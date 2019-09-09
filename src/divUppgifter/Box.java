cbe1f72903213b0fbb3de8efc8097f35fa9931fd
SpikeYr <emil.f.anderson@gmail.com>
2019-09-09 11:00:02 +0200

    09-02 Uppgift 5

--- src/divUppgifter/Box.java
+++ src/divUppgifter/Box.java
@@ -3,15 +3,29 @@ package divUppgifter;
 import java.util.ArrayList;
 
 public class Box {
-	
+
 	public ArrayList<Object> a = new ArrayList<Object>();
-	
+
 	public Box() {
-		
+
 	}
-	
+
 	public <E> void add(E e) {
 		a.add(e);
 	}
-	
+
+	public Object get(int i) {
+		return a.get(i);
+	}
+
+	public <E1, E2> boolean compare(E1 e1, E2 e2) {
+
+		if (e1.getClass() == e2.getClass()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
 }
