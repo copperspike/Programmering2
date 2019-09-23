629c4602c840fabca964560a3d53b17717d963f5
SpikeYr <emil.f.anderson@gmail.com>
2019-09-23 10:49:21 +0200

    WindowDemo Commit 2
    Förökar antal knappar

--- src/divUppgifter/WindowDemo.java
+++ src/divUppgifter/WindowDemo.java
@@ -13,12 +13,11 @@ import javafx.stage.Stage;
 public class WindowDemo extends Application implements EventHandler<ActionEvent> {
 
 	double width, height;
-	Button knapp1;
+	Button[] knappar;
 	Scene scene;
 	Group root;
 	Rectangle bg;
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch();
@@ -26,41 +25,50 @@ public class WindowDemo extends Application implements EventHandler<ActionEvent>
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
+		knappar = new Button[3];
 		width = 300;
 		height = 300;
-		knapp1 = new Button("OK");
-		knapp1.setOnAction(this);
 		bg = new Rectangle(width, height, Color.BLACK);
 		root = new Group();
 		scene = new Scene(root, width, height);
-		
-		knapp1.setMinSize(width / 6.0, height / 6.0);
-		knapp1.setTranslateX(width / 2.0 - width /12.0);
-		knapp1.setTranslateY(height / 2.0 - height /12.0);
-		
+
+		for (int i = 0; i < knappar.length; i++) {
+			knappar[i] = new Button("OK");
+			knappar[i].setOnAction(this);
+
+			knappar[i].setMinSize(width / 6.0, height / 6.0);
+			knappar[i].setTranslateX(width / 2.0 - width / 12.0);
+			knappar[i].setTranslateY(height / 2.0 - height / 12.0);
+		}
+
 		root.getChildren().add(bg);
-		root.getChildren().add(knapp1);
-		
+		root.getChildren().add(knappar[0]);
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
-		if (event.getSource() == knapp1) {
-			root.getChildren().clear();
+		root.getChildren().clear();
+
+		if (event.getSource() == knappar[0]) {
 			
-			if (!bg.getFill().equals(Color.RED)) {
-				bg.setFill(Color.RED);
-				root.getChildren().add(bg);
-				root.getChildren().add(knapp1);
-			} else {
-				bg.setFill(Color.BLACK);
-				root.getChildren().add(bg);
-				root.getChildren().add(knapp1);
-			}
+			bg.setFill(Color.RED);
+			root.getChildren().add(bg);
+			root.getChildren().add(knappar[1]);
+		} else if (event.getSource() == knappar[1]) {
+			
+			bg.setFill(Color.YELLOW);
+			root.getChildren().add(bg);
+			root.getChildren().add(knappar[2]);
+		} else if (event.getSource() == knappar[2]) {
+
+			bg.setFill(Color.BLACK);
+			root.getChildren().add(bg);
+			root.getChildren().add(knappar[0]);
 		}
 	}
 
