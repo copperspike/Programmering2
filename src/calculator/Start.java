f502dbcf2419922ae3fe21fce5ec906953a36335
SpikeYr <emil.f.anderson@gmail.com>
2019-09-30 11:00:34 +0200

    Calculator start class commit 1

--- src/calculator/Start.java
+++ src/calculator/Start.java
@@ -1,9 +1,18 @@
 package calculator;
 
-public class Start {
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Start extends Application {
 
 	public static void main(String[] args) {
-		
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		GridPane grid = new GridPane();
 		
 		
 	}
