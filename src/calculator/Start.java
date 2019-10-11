57f066c432b12f68ed90dfa91fe0e73b30becb69
SpikeYr <emil.f.anderson@gmail.com>
2019-10-11 15:05:05 +0200

    Calculator Start commit 4 - Comments and content
    Added comments to relevant places.
    Removed that which is now in the interface Layout.
    Added everything but the action for the button '='.

--- src/calculator/Start.java
+++ src/calculator/Start.java
@@ -1,21 +1,12 @@
 package calculator;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class Start extends Application {
-
-	int width = 4;
-	int height = 6;
+public class Start extends Application implements Layout {
 
 	public static void main(String[] args) {
 		launch();
@@ -23,35 +14,119 @@ public class Start extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		HBox root = new HBox();
-		Scene scene = new Scene(root, 100 * (width + 1), 100 * (height + 1), Color.WHITE);
+
+		// Creates and shows the window the calculator will be placed on
+		root.getChildren().add(border);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-		BorderPane border = new BorderPane();
-		root.getChildren().add(border);
+		// Modifies some properties of the TextField show then adds it to the top of the BorderPane border
+		show.setFont(Font.font("Verdana", 32));
+		show.setPrefHeight(20 * height);
+		show.setText("");
+
+		// Adds margins and content to the BorderPane border
+		border.setTop(show);
 		border.setLeft(new Rectangle(50, 0));
 		border.setRight(new Rectangle(50, 0));
-
-		GridPane numpad = new GridPane();
+		border.setBottom(new Rectangle(0, 50));
 		border.setCenter(numpad);
 
-		TextField show = new TextField();
-		show.setFont(Font.font("Verdana", 32));
+		// Declares a String array for the buttons on the grid
+		String[] numpadKeys = { "\u221A", "\u00B2", "(", ")", "+", "-", "\u00D7", "\u00f7", "1", "2", "3", "mod", "4",
+				"5", "6", "abs", "7", "8", "9", "C", ".", "0", "=" };
 
-		show.setText("\u221A");
-		border.setTop(show);
-
-		String[] numpadKeys = { "+", "-", "\u00f7", "\u00D7", "1", "2", "3", "C", "4", "5", "6"};
-
+		// Defines, places, and sets the action for all buttons, then adds them to the grid
 		for (int i = 0; i < numpadKeys.length; i++) {
-			Button temp = new Button(numpadKeys[i]);
+
+			// Using a local variable makes the usage of updateInput() a lot easier
+			String key = new String(numpadKeys[i]);
+
+			// placeSet & actionSet are made so that buttons with unusual placement or
+			// actions aren't redefined at the end of the for loop
+			boolean placeSet = false;
+			boolean actionSet = false;
+
+			// Defines the button that will be added to the grid
+			Button temp = new Button(key);
 			temp.setFont(Font.font("Verdana", 28));
-			temp.setPrefSize(10000, 100);
-			
-			numpad.add(temp, i % 4, (int) Math.floor(i / 4.0));
+			temp.setPrefSize(200, 150);
+
+			// Defines the placement and/or action for buttons with unusual ones
+			if (key.equals("0")) {
+				temp.setPrefWidth(400);
+				numpad.add(temp, i % 4, (int) Math.floor(i / 4.0), 2, 1);
+				placeSet = true;
+			} else
+
+			if (key.equals("=")) {
+				numpad.add(temp, i % 4 + 1, (int) Math.floor(i / 4.0));
+				placeSet = true;
+			} else
+
+			if (key.equals("\u00D7")) {
+				temp.setOnAction(event -> {
+					updateShow("*");
+				});
+				actionSet = true;
+			} else
+
+			if (key.equals("\u00f7")) {
+				temp.setOnAction(event -> {
+					updateShow("/");
+				});
+				actionSet = true;
+			} else
+
+			if (key.equals("mod")) {
+				temp.setOnAction(event -> {
+					updateShow("%");
+				});
+				actionSet = true;
+			} else
+
+			if (key.equals("abs")) {
+				temp.setOnAction(event -> {
+					updateShow("|");
+				});
+				actionSet = true;
+			} else
+
+			if (key.equals("C")) {
+				temp.setOnAction(event -> {
+					show.clear();
+				});
+				actionSet = true;
+			} else
+
+			if (key.equals("=")) {
+				temp.setOnAction(event -> {
+
+				});
+				actionSet = true;
+			}
+
+			// Defines the standard placement and action for buttons
+			if (!actionSet) {
+				temp.setOnAction(event -> {
+					updateShow(key);
+				});
+			}
+			if (!placeSet) {
+				numpad.add(temp, i % 4, (int) Math.floor(i / 4.0));
+			}
+
 		}
 
+	}
+
+	/**
+	 * Appends the String input to the TextField show
+	 * 
+	 * @param input
+	 */
+	private void updateShow(String input) {
+		show.textProperty().set(show.textProperty().get() + input);
 	}
 
 }
