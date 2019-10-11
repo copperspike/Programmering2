1c437c1c03bc96dfeb811509097dd16d15e84721
SpikeYr <emil.f.anderson@gmail.com>
2019-10-11 14:58:48 +0200

    Calculator Layout interface commit 0
    Created an interface for general variables that are relevant for the calculator.
    
    This is done both for ease and an A

---
+++ src/calculator/Layout.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
+interface Layout {
+
+	int width = 6;
+	int height = 4;
+	TextField show = new TextField();
+	HBox root = new HBox();
+	Scene scene = new Scene(root, 100 * (width + 1), 100 * (height + 1.5), Color.WHITE);
+	BorderPane border = new BorderPane();
+	public GridPane numpad = new GridPane();
+	
+}
