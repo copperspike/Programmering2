65e2e4e84dab3b797444cef53e201f0adc68c959
SpikeYr <emil.f.anderson@gmail.com>
2019-10-21 10:29:47 +0200

    Calculator AlgebraicProcessing commit 3 - General layout & simple methods

--- src/calculator/AlgebraicProcessing.java
+++ src/calculator/AlgebraicProcessing.java
@@ -1,11 +1,68 @@
 package calculator;
 
+import java.util.HashMap;
+
+@SuppressWarnings("unused")
 public class AlgebraicProcessing {
 
 	public static String Equate (String input) {
 		
-		return null;
+		String ordered = Parenthesize(input);
 		
+		return new String(calculate(ordered) + "");
+	}
+	
+	private static String Parenthesize (String in) {
+		
+		
+		return null;
+	}
+	
+	private static double calculate(String ordered) {
+		
+		HashMap<Character, Integer> hmap = new HashMap<Character, Integer>();
+		
+		for (int i = 0; i < ordered.length(); i++) {
+			
+		}
+		
+		return 0;
+	}
+	
+	private static double add(double val1, double val2) {
+		return val1 + val2;
+	}
+	
+	private static double subtract(double val1, double val2) {
+		return val1 - val2;
+	}
+	
+	private static double multiply(double val1, double val2) {
+		return val1 * val2;
+	}
+	
+	private static double divide(double val1, double val2) {
+		return val1 / val2;
+	}
+	
+	private static double square(double val) {
+		return val * val;
+	}
+	
+	private static double root(double val) {
+		return Math.sqrt(val);
+	}
+	
+	private static double mod(double val1, double val2) {
+		return val1 % val2;
+	}
+	
+	private static double abs(double val) {
+		if (val < 0) {
+			return val * -1;
+		} else {
+			return val;
+		}
 	}
 	
 }
