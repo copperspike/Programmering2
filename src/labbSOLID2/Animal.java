9f88510256e920395c7f9cbdc3c89660ba41f186
SpikeYr <emil.f.anderson@gmail.com>
2019-11-29 14:04:14 +0100

    Labb 2 SOLID - Liscov

---
+++ src/labbSOLID2/Animal.java
@@ -0,0 +1,25 @@
+package labbSOLID2;
+
+public abstract class Animal {
+
+	private String _color;
+
+	public Animal(String color) {
+		_color = color;
+	}
+
+	public String getColor() {
+		return _color;
+	}
+
+	public void paint(String color) {
+		_color = color;
+	}
+
+	abstract void speak();
+
+	abstract void sleep();
+
+	abstract void eat();
+
+}
