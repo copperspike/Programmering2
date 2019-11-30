0dcbc94832bdbc9558a8f738fc93a694582710e9
Demikejunior <42673940+Demikejunior@users.noreply.github.com>
2019-11-30 01:46:08 +0100

    Cash Register commit 0 - creation

---
+++ src/cashRegister/Main.java
@@ -0,0 +1,17 @@
+package cashRegister;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+
+    }
+}
