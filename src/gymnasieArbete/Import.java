0dea9c3f84c639ded2b61cc04929c26d9add2bf1
SpikeYr <emil.f.anderson@gmail.com>
2020-01-09 09:35:16 +0100

    GYARB - Import Commit 1

--- src/gymnasieArbete/Import.java
+++ src/gymnasieArbete/Import.java
@@ -1,8 +1,25 @@
 package gymnasieArbete;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Import {
+	public ArrayList<String[]> getSheet() {
+		return sheet;
+	}
+
+	private ArrayList<String[]> sheet = new ArrayList<String[]>();
 
 	public Import(String loc) {
+		Scanner sc = new Scanner(loc);
 
+		int index = 0;
+		while (sc.hasNextLine()) {
+			sheet.add(sc.nextLine().split(";"));
+
+			index++;
+		}
+
+		sc.close();
 	}
 }
