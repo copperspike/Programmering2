6730b94131d434fcfc019a178bdaf63a70510eff
SpikeYr <emil.f.anderson@gmail.com>
2020-01-15 12:17:42 +0100

    GYARB - Import Commit 2

--- src/gymnasieArbete/Import.java
+++ src/gymnasieArbete/Import.java
@@ -4,21 +4,52 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Import {
-	public ArrayList<String[]> getSheet() {
+	public ArrayList<ArrayList<String>> getSheet() {
 		return sheet;
 	}
 
-	private ArrayList<String[]> sheet = new ArrayList<String[]>();
+	public void clearSheet() {
+		sheet.clear();
+	}
 
-	public Import(String loc) {
+	private ArrayList<ArrayList<String>> sheet = new ArrayList<ArrayList<String>>();
+
+	/**
+	 * loc is the location of the .csv file that is to be imported
+	 * target is which part of the spreadsheet that is supposed to be imported,
+	 * target == 0 targets the data points, and the minimum value, maximum value and the data type of those data points
+	 * target == 1 targets the objects
+	 *
+	 * @param loc
+	 * @param target
+	 */
+	public Import(String loc, boolean target) {
+
+		clearSheet();
 		Scanner sc = new Scanner(loc);
 
-		int index = 0;
-		while (sc.hasNextLine()) {
-			sheet.add(sc.nextLine().split(";"));
+		if (!target) {
+			while (sc.hasNextLine()) {
+				ArrayList<String> out = new ArrayList<String>();
+				String[] temp = sc.nextLine().split(";", 4);
 
-			index++;
+				for (String in: temp) {
+					out.add(in);
+				}
+				
+				sheet.add(out);
+			}
+		} else {
+			int index = 0;
+			while (sc.hasNextLine()) {
+				ArrayList<ArrayList<String>> objectArray = new ArrayList<ArrayList<String>>();
+				String[] temp = sc.nextLine().split(";");
+				
+
+
+			}
 		}
+
 
 		sc.close();
 	}
