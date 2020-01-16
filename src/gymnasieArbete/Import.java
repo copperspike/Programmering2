4df73c83f9c4f924c6ba3fd443c4d8db837e3e48
SpikeYr <emil.f.anderson@gmail.com>
2020-01-16 11:24:06 +0100

    GYARB - Import Commit 3
    Now functional!!!

--- src/gymnasieArbete/Import.java
+++ src/gymnasieArbete/Import.java
@@ -1,5 +1,7 @@
 package gymnasieArbete;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -17,40 +19,96 @@ public class Import {
 	/**
 	 * loc is the location of the .csv file that is to be imported
 	 * target is which part of the spreadsheet that is supposed to be imported,
-	 * target == 0 targets the data points, and the minimum value, maximum value and the data type of those data points
-	 * target == 1 targets the objects
+	 * target == false targets the data points, and the minimum value, maximum value and the data type of those data points
+	 * target == true targets the objects
 	 *
 	 * @param loc
 	 * @param target
 	 */
-	public Import(String loc, boolean target) {
+	public Import(String loc, boolean target) throws FileNotFoundException {
 
 		clearSheet();
-		Scanner sc = new Scanner(loc);
+		Scanner sc = new Scanner(new File(loc));
 
 		if (!target) {
 			while (sc.hasNextLine()) {
-				ArrayList<String> out = new ArrayList<String>();
-				String[] temp = sc.nextLine().split(";", 4);
 
-				for (String in: temp) {
-					out.add(in);
-				}
-				
-				sheet.add(out);
+				sheet.add(HorizontalInsert(sc.nextLine().split(";"), 0, 4));
+
 			}
+
 		} else {
-			int index = 0;
-			while (sc.hasNextLine()) {
-				ArrayList<ArrayList<String>> objectArray = new ArrayList<ArrayList<String>>();
-				String[] temp = sc.nextLine().split(";");
-				
+			int dataIndex = 0;
+			ArrayList<ArrayList<String>> objectArray = new ArrayList<ArrayList<String>>();
 
+			while (sc.hasNextLine()) {
+
+				ArrayList<String> dataPoint = HorizontalInsert(sc.nextLine().split(";"), 4, 0);
+
+				for (int objectIndex = 0; objectIndex < dataPoint.size(); objectIndex++) {
+
+					ArrayList<String> arrayObject = new ArrayList<String>();
+
+					if (dataIndex == 0) {
+
+						arrayObject.add(dataPoint.get(objectIndex));
+
+						objectArray.add(arrayObject);
+
+					} else {
+
+						arrayObject.addAll(objectArray.get(objectIndex));
+
+						arrayObject.add(dataPoint.get(objectIndex));
+
+						objectArray.set(objectIndex, arrayObject);
+
+					}
+
+
+				}
+
+				dataIndex++;
 
 			}
+
+			for (ArrayList<String> out: objectArray) {
+
+				sheet.add(out);
+
+			}
+
 		}
 
 
 		sc.close();
 	}
+
+	private ArrayList<String> HorizontalInsert(String[] in, int start, int end) {
+		ArrayList<String> arrayObject = new ArrayList<String>();
+
+		if (end == 0) {
+
+			for (int i = start; i < in.length; i++) {
+				String dataPoint = in[i];
+
+				arrayObject.add(dataPoint);
+			}
+
+			return arrayObject;
+
+		} else {
+
+			for (int i = start; i < end; i++) {
+				String dataPoint = in[i];
+
+				arrayObject.add(dataPoint);
+			}
+
+			return arrayObject;
+
+		}
+
+	}
+
 }
